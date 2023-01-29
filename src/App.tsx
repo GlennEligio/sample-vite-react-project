9455067140fd79d609de48dca938aa35edd54f08
John Glenn ACN laptop <glenneligio@gmail.com>
2023-01-29 21:13:14 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <form action="">
+      <input
+        type="text"
+        id="dinosour name"
+        className="dinosaur-input"
+        aria-describedby="dinosaur-name-info"
+      />
+    </form>
+  );
+}
+
+export default App;
